ea232fd2dea29e89b9e187b8d10a84fab08f6a94
Yeren Daniyar <y.daniyar.official@gmail.com>
2023-12-25 00:33:00 +0600

    add lesson1:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 15;
+int b = 7;
+int c = 4;
+int d = 9;
+int e = 11;
+
+int max = a;
+
+if (b > max)
+{
+    max =   b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
