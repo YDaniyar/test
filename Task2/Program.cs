d504cb37b3eb118677ac65b37eaaa7805a2d3e8f
Yeren Daniyar <y.daniyar.official@gmail.com>
2023-12-25 00:05:02 +0600

    add lesson1:Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -9,6 +9,7 @@ Console.WriteLine(sum2);
 
 double d1 = 5.5;
 double sum3 = d1 + i1;
+Console.WriteLine(sum3);
 
 int product1 = i1 * i2;
 long product2 = l1 * i1;
@@ -16,3 +17,10 @@ double product3 = i1 * d1;
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+int quotient1 = i1 / i2;
+Console.WriteLine(quotient1);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);
